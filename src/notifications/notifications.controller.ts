01a2c9b81331fd4ad8588d3e5fd62267072982f8
hospital <hospital@ya.ru>
2026-05-07 16:46:05 +0300

    fix notification

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -11,33 +11,48 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
+
 import { NotificationsService } from './notifications.service';
+
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('notifications')
-@UseGuards(AuthGuard)
 export class NotificationsController {
   constructor(private service: NotificationsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(
     @CurrentUser() user,
-    @Body() dto: { goalId?: string; times: string[] },
+    @Body()
+    dto: {
+      goalId?: string;
+      times: string[];
+    },
   ) {
     return this.service.create(user.id, dto.goalId || null, dto.times);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: { times: string[] }) {
+  update(
+    @Param('id') id: string,
+    @Body()
+    dto: {
+      times: string[];
+    },
+  ) {
     return this.service.update(id, dto.times);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   delete(@Param('id') id: string) {
     return this.service.delete(id);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   get(@CurrentUser() user) {
     return this.service.get(user.id);
